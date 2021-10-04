945150760c58b1c978e1cf3b1bc4c2f6c833acb0
ZahirBilal <87704717+ZahirBilal@users.noreply.github.com>
2021-10-04 18:59:12 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binom_gaussian_lib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['binom_gaussian_lib'],
+      Author = 'Zahir Bilal',
+      Author_Email = 'zaherbilal93@gmail.com',
+      zip_safe=False)
